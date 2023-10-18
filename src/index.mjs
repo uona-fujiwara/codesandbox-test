bad224d1fc451e1100f1e0833e72aeabedbc71ef
うおな <119503862+uona-fujiwara@users.noreply.github.com>
2023-10-18 12:43:35 +0000

    2023/10/18 勉強分

--- src/index.mjs
+++ src/index.mjs
@@ -167,3 +167,44 @@ const nameArr = ["田中", "山田", "鈴木"];
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// const val1 = 1 > 0 ? "true" : "false";
+// console.log(val1);
+
+// const num = "1300";
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+
+// console.log(checkSum(51, 49));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("inside ||");
+// }
+// if (flag1 && flag2) {
+//   console.log("inside &&");
+// }
+
+// 左辺がfalseなら右辺を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+//左辺がtrueなら右辺を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
